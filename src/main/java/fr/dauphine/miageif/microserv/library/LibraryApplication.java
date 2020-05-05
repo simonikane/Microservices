e480a2d71b43ac60f5ae0262f295401c904aa151
Simona Arizanova <simona.arizanova@gmail.com>
2020-05-05 19:42:37 +0200

    Ajout de commentaire

--- src/main/java/fr/dauphine/miageif/microserv/library/LibraryApplication.java
+++ src/main/java/fr/dauphine/miageif/microserv/library/LibraryApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//Test
 public class LibraryApplication {
 
     public static void main(String[] args) {
