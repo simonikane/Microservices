82392a623b1fd8144ee34c4c0bb8c1449fe114aa
axelacu <axel.acuna@dauphine.eu>
2020-05-05 19:35:42 +0200

    Lancement Projet

---
+++ src/main/java/fr/dauphine/miageif/microserv/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package fr.dauphine.miageif.microserv.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
+
+}
